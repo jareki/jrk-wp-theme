897d93d8114f1b2d7d991644ca431da3bbacb007
jareki <yayarikya@gmail.com>
2024-04-16 13:09:03 +0300

    export theme edits

---
+++ patterns/search-start.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Search Start Page Pattern
+ * Slug: jrk/search-start
+ * Categories: content
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:post-title {"level":4} /-->
+
+<!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true} /--></div>
+<!-- /wp:group -->
---
+++ patterns/search.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Search Page Pattern
+ * Slug: jrk/search
+ * Categories: content
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:query-title {"type":"search","level":4} /-->
+
+<!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /--></div>
+<!-- /wp:group -->
